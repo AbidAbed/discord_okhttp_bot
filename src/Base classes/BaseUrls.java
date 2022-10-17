de587154a84bd47e66e11b828251e6cdfd0331f5
aabedaabed075 <aabedaabed075@gmail.com>
2022-10-17 14:28:24 +0300

    base class created guild operations class created

--- src/Base classes/BaseUrls.java
+++ src/Base classes/BaseUrls.java
@@ -9,11 +9,11 @@ class BaseUrls {
         this.guildsUrl = guildsUrl;
         this.channelsUrl = channelsUrl;
         this.meUrl = meUrl;
-        this.token = "Bot OTgwMTk1NjU4NDI4NDY1MTYz.GYNn9J.PNtTODXGj55m69feaOI2W8oXPtxboV6HZ3kC7g";
+        this.token = "";
     }
 
     public BaseUrls(String guildId) {
-        this.token = "Bot OTgwMTk1NjU4NDI4NDY1MTYz.GYNn9J.PNtTODXGj55m69feaOI2W8oXPtxboV6HZ3kC7g";
+        this.token = "";
         this.guildsUrl = "https://discord.com/api/v10/guilds"
         this.channelsUrl = "https://discord.com/api/v9/guilds/"+guildId+"/channels";
         this.meUrl = "https://discord.com/api/v10/users/@me";
